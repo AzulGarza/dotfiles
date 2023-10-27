2ce43dd15d423aad9fe465a598bb79fdc1b1a1c6
FedericoGarza <fede.garza.ramirez@gmail.com>
2023-10-26 20:19:57 -0700

    feat: add markdown plugin

--- neovim/custom/plugins.lua
+++ neovim/custom/plugins.lua
@@ -78,5 +78,11 @@ local plugins = {
     'Exafunction/codeium.vim',
     event = 'BufEnter'
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
 return plugins
